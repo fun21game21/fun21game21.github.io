0b33dee3550c0caf5f655838d736ffb50f239621
max1mka <146134223+fun21game21@users.noreply.github.com>
2024-06-24 13:50:55 +0300

    Update scrips.js
    TonBlocks

--- main.js
+++ main.js
@@ -1,58 +1,25 @@
-function getChatIdFromUrl() {
-  const url = window.location.href;
-  const match = url.match(/\?(\d+)/);
-  return match ? match[1] : null;
-}
+let isDragging = false;
+let startX;
+let scrollLeft;
 
-
-
-// Функция для отправки сигнала телеграм-боту
-function sendTelegramSignal() {
-              //const botToken = '6487748195:AAGjyQZW6IAt3RuaU88u3HxZDkmkFpBUb1U';
-              const signalUrl = 'https://127.0.0.1:8908/user_activity_signal';
-              const chatId = Number(getChatIdFromUrl());
-              // Отправляем POST-запрос на сервер Flask
-              fetch(signalUrl, {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({ chat_id: chatId }),
-              })
-              .then(response => response.json())
-              .then(data => console.log(data))
-              .catch(error => console.error('Error:', error));
-            }
-
-// Динамический таймер в title и отправку сигнала через 3 секунды
-function startTimer(duration) {
-    let timer = duration;
-    let timerInterval = setInterval(function () {
-        let minutes = parseInt(timer / 60, 10);
-        let seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        document.title = `Таймер: ${minutes}:${seconds}`;
-
-        if (--timer < 0) {
-            clearInterval(timerInterval);
-            document.title = "Вознаграждение зачислено!";
-
-            
-            sendTelegramSignal();
-        }
-    }, 1000);
-}
-// Кнопка
-document.getElementById('completeAdButton').addEventListener('click', function() {
-    sendTelegramSignal();
-
-    window.close();
+document.querySelector('.card-container').addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startX = e.pageX - e.target.offsetLeft;
+  scrollLeft = e.target.scrollLeft;
 });
 
-window.onload = function () {
-    let duration = 3;
-    startTimer(duration);
-};
+document.querySelector('.card-container').addEventListener('mouseleave', () => {
+  isDragging = false;
+});
+
+document.querySelector('.card-container').addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+document.querySelector('.card-container').addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX - e.target.offsetLeft;
+  const walk = x - startX;
+  e.target.scrollLeft = scrollLeft - walk;
+});
